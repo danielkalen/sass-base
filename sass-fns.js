2b2045be2cb3f4df1b814ee00cdebf8d387c46e6
danielkalen <d@danielkalen.com>
2016-05-16 15:28:17 -0400

    Separate from sandbox

---
+++ sass-fns.js
@@ -0,0 +1,19 @@
+var fs = require('fs'),
+	path = require('path'),
+	sass = require('node-sass'),
+	functions = function(fontLocation){
+		fontLocation = fontLocation || '/public/fonts/';
+	
+		return {
+			'base64Font($file)': function(file) {
+				var filePath = __dirname+fontLocation+(file.getValue()),
+					output, data;
+				
+				data = fs.readFileSync(filePath);
+				output = data.toString('base64');
+				return new sass.types.String(output);
+			}
+		}
+	};
+
+module.exports = functions;
