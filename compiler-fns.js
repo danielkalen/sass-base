2ebcfce1d27347affdf9b03aa44331ad84f15a0d
danielkalen <d@danielkalen.com>
2016-11-16 11:02:43 -0500

    [Fix] Compiler Fns - properly export functions && use process.cwd as path base instead of __dirname

--- compiler-fns.js
+++ compiler-fns.js
@@ -2,17 +2,14 @@ var fs = require('fs'),
 	path = require('path'),
 	sass = require('node-sass');
 
-module.exports = function(fontLocation){
-	fontLocation = fontLocation || process.env.fontsDir || '/public/fonts/';
-
-	return {
-		'base64Font($file)': function(file) {
-			var filePath = path.join(__dirname, fontLocation, file.getValue()),
-				output, data;
-			
-			data = fs.readFileSync(filePath);
-			output = data.toString('base64');
-			return new sass.types.String(output);
-		}
+module.exports = {
+	'base64Font($file)': function(file) {
+		var fontLocation = process.env.fontsDir || '/public/fonts/',
+			filePath = path.join(process.cwd(), fontLocation, file.getValue()),
+			output, data;
+		
+		data = fs.readFileSync(filePath);
+		output = data.toString('base64');
+		return new sass.types.String(output);
 	}
 };
