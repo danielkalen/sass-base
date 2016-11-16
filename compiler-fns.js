641156d2c21f378948a77b84210d02b74d6e8578
danielkalen <d@danielkalen.com>
2016-11-16 10:03:45 -0500

    ""

--- compiler-fns.js
+++ compiler-fns.js
@@ -1,19 +1,18 @@
 var fs = require('fs'),
 	path = require('path'),
 	sass = require('node-sass'),
-	functions = function(fontLocation){
-		fontLocation = fontLocation || process.env.fontsDir || '/public/fonts/';
-	
-		return {
-			'base64Font($file)': function(file) {
-				var filePath = path.join(__dirname, fontLocation, file.getValue()),
-					output, data;
-				
-				data = fs.readFileSync(filePath);
-				output = data.toString('base64');
-				return new sass.types.String(output);
-			}
-		}
-	};
 
-module.exports = functions;
+module.exports = function(fontLocation){
+	fontLocation = fontLocation || process.env.fontsDir || '/public/fonts/';
+
+	return {
+		'base64Font($file)': function(file) {
+			var filePath = path.join(__dirname, fontLocation, file.getValue()),
+				output, data;
+			
+			data = fs.readFileSync(filePath);
+			output = data.toString('base64');
+			return new sass.types.String(output);
+		}
+	}
+};
